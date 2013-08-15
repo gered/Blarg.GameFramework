32f9203cee54d1397ce1596c386b03721fbcf8ff
gered <gered@blarg.ca>
2013-08-15 19:42:49 -0400

    add ScreenOrientation

---
+++ Blarg.GameFramework/Graphics/ScreenOrientation.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Blarg.GameFramework.Graphics
+{
+	public enum ScreenOrientation
+	{
+		Rotation0 = 0,
+		Rotation90 = 90,
+		Rotation180 = 180,
+		Rotation270 = 270
+	}
+}
