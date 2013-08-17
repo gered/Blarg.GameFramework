c7b00469ae992dde14dbe580faf3f6138d760c6d
gered <gered@blarg.ca>
2013-08-17 18:10:36 -0400

    forgot to remove these too

--- Blarg.GameFramework.SDL2/SDLWindow.cs
+++ Blarg.GameFramework.SDL2/SDLWindow.cs
@@ -7,10 +7,6 @@ namespace Blarg.GameFramework
 		public Rect ClientRectangle { get; internal set; }
 		public int ClientWidth { get; internal set; }
 		public int ClientHeight { get; internal set; }
-		public int X { get; internal set; }
-		public int Y { get; internal set; }
-		public int Width { get; internal set; }
-		public int Height { get; internal set; }
 	}
 }
 
