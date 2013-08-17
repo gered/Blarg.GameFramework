ba5872a1de53e24f1ad7368218e956e0b92aaf39
gered <gered@blarg.ca>
2013-08-17 17:48:14 -0400

    don't really need the full (external) window dimensions, just the client-area stuff

--- Blarg.GameFramework/IPlatformWindow.cs
+++ Blarg.GameFramework/IPlatformWindow.cs
@@ -7,10 +7,6 @@ namespace Blarg.GameFramework
 		Rect ClientRectangle { get; }
 		int ClientWidth { get; }
 		int ClientHeight { get; }
-		int X { get; }
-		int Y { get; }
-		int Width { get; }
-		int Height { get; }
 	}
 }
 
