fb7b85d6ab5cfb210c834c2f04b5d1195a51cf82
gered <gered@blarg.ca>
2013-08-28 23:52:57 -0400

    add basic Tile serialize/deserialize to binary methods

---
+++ Blarg.GameFramework/TileMap/TileDataSerializer.cs
@@ -0,0 +1,41 @@
+using System;
+using System.IO;
+
+namespace Blarg.GameFramework.TileMap
+{
+	public static class TileDataSerializer
+	{
+		public static void Serialize(Tile src, BinaryWriter writer)
+		{
+			writer.Write(src.TileIndex);
+			writer.Write(src.Flags);
+			writer.Write(src.TileLight);
+			writer.Write(src.SkyLight);
+			writer.Write(src.Rotation);
+			writer.Write(src.ParentTileOffsetX);
+			writer.Write(src.ParentTileOffsetY);
+			writer.Write(src.ParentTileOffsetZ);
+			writer.Write(src.ParentTileWidth);
+			writer.Write(src.ParentTileHeight);
+			writer.Write(src.ParentTileDepth);
+			writer.Write(src.Color);
+		}
+
+		public static void Deserialize(BinaryReader reader, Tile dest)
+		{
+			dest.TileIndex = reader.ReadInt16();
+			dest.Flags = reader.ReadInt16();
+			dest.TileLight = reader.ReadByte();
+			dest.SkyLight = reader.ReadByte();
+			dest.Rotation = reader.ReadByte();
+			dest.ParentTileOffsetX = reader.ReadByte();
+			dest.ParentTileOffsetY = reader.ReadByte();
+			dest.ParentTileOffsetZ = reader.ReadByte();
+			dest.ParentTileWidth = reader.ReadByte();
+			dest.ParentTileHeight = reader.ReadByte();
+			dest.ParentTileDepth = reader.ReadByte();
+			dest.Color = reader.ReadInt32();
+		}
+	}
+}
+
