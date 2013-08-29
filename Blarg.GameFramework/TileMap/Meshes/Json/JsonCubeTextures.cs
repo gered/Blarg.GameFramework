420f134654ec66155c8022f6abb9c80b4bdc1074
gered <gered@blarg.ca>
2013-08-28 23:26:02 -0400

    type changes

--- Blarg.GameFramework/TileMap/Meshes/Json/JsonCubeTextures.cs
+++ Blarg.GameFramework/TileMap/Meshes/Json/JsonCubeTextures.cs
@@ -4,12 +4,12 @@ namespace Blarg.GameFramework.TileMap.Meshes.Json
 {
 	public class JsonCubeTextures
 	{
-		public int Top;
-		public int Bottom;
-		public int Front;
-		public int Back;
-		public int Left;
-		public int Right;
+		public int? Top;
+		public int? Bottom;
+		public int? Front;
+		public int? Back;
+		public int? Left;
+		public int? Right;
 	}
 }
 
--- Blarg.GameFramework/TileMap/Meshes/Json/JsonTileDefinition.cs
+++ Blarg.GameFramework/TileMap/Meshes/Json/JsonTileDefinition.cs
@@ -8,20 +8,20 @@ namespace Blarg.GameFramework.TileMap.Meshes.Json
 	{
 		public bool Cube;
 		public JsonCubeTextures Textures;
-		public int Texture;
+		public int? Texture;
 		public List<string> Faces;
 		public string Model;
 		public List<JsonTileSubModel> Models;
 		public string CollisionModel;
 		public string CollisionShape;
 		public List<string> OpaqueSides;
-		public int Light;
+		public byte Light;
 		public bool Alpha;
 		public float Translucency;
-		public Color Color;
-		public Vector3 ScaleToSize;
-		public Vector3 PositionOffset;
-		public Vector3 CollisionPositionOffset;
+		public Color? Color;
+		public Vector3? ScaleToSize;
+		public Vector3? PositionOffset;
+		public Vector3? CollisionPositionOffset;
 	}
 }
 
